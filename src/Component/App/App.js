04639dad70a00981651b700a054a74c72f47d644
Loryn Mason <masonloryn@gmail.com>
2018-12-04 14:42:09 -0700

    Style splash page and add sass

--- src/Component/App/App.js
+++ src/Component/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-// import './styles/main.scss';
-import { fetchScroll } from '../../../src/helper.js'
+import '../styles/main.scss';
+import { fetchScroll } from '../../helper.js';
+import Nav from '../Nav/Nav.js'
 
 class App extends Component {
   constructor() {
@@ -9,7 +10,6 @@ class App extends Component {
       films: []
     }
   }
-
   async componentDidMount() {
     const films = await fetchScroll()
     this.setState({
@@ -18,11 +18,35 @@ class App extends Component {
   }
   
   render() {
+    const text = this.state.films.map((film) => {
+      return film.text
+    })[0]
     return (
       <div className="App">
-        
+      <section className='background'>
+      <div className="fade">
+        <div className="logo">
+        <img alt='StarWars logo' src="http://pngimg.com/uploads/star_wars_logo/star_wars_logo_PNG29.png"/> 
+        </div>
+        </div>
+        <div className="crawl">
+        {text}
+        </div>
+      </section>
       </div>
     );
+    // return (
+    //   <div className="App">
+    //     <header>
+    //       <Nav />
+    //       <div className="logo">
+    //         <img alt='StarWars logo'                       
+    //              src="http://pngimg.com/uploads/star_wars_logo/star_wars_logo_PNG29.png"/>
+    //       </div>
+    //     </header>
+    //   </div>
+    // );
+
   }
 }
 
---
+++ src/Component/Nav/Nav.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../styles/main.scss';
+import PropTypes from 'prop-types'
+
+const Nav = () => {
+
+  return (
+    <nav>
+      <button>Characters</button>
+      <button>Planets</button>
+      <button>Vehicles</button>
+  
+    </nav>
+  )
+}
+
+export default Nav
