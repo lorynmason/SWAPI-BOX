0615797dfa559620c0dfd6177d4189d17b9049c7
Loryn Mason <masonloryn@gmail.com>
2018-12-04 17:38:44 -0700

    Set up app for itiration 2

---
+++ src/Component/CardContainer/CardContainer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../styles/main.scss';
+import PropTypes from 'prop-types'
+
+const CardContainer = ( {favorites, changePage} ) => {
+
+  return (
+  <section>
+  </section>
+  )
+}
+
+CardContainer.propTypes = {
+  favorites: PropTypes.array.isRequired
+}
+
+export default CardContainer
