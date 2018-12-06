db7a4beca8337ee546927c472239f312762877af
Sara Ebbers <johndoe@example.com>
2018-12-06 08:01:07 -0700

    Create new files for tests, moved from old helper file, helper files to be deleted

---
+++ src/apiCalls.test.js
@@ -0,0 +1,16 @@
+import * as API from './apiCalls.js';
+
+describe('fetchScroll', () => {
+
+  it.skip('calls fetch with the correct params', () => {
+
+  })
+
+  it.skip('calls cleanFilmsData', () => {
+    let cleanFilmsData = jest.fn();
+    fetchScroll();
+    expect(cleanFilmsData).toHaveBeenCalled()
+  })
+
+
+})
