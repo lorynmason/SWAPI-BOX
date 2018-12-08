db9184c7155ea809a0c0c9b80074bcf3091d1978
Loryn Mason <masonloryn@gmail.com>
2018-12-08 15:22:18 -0700

    Linted Splash.js

--- src/Component/Splash/Splash.js
+++ src/Component/Splash/Splash.js
@@ -1,46 +1,44 @@
-import React from 'react';
-import '../styles/main.scss';
-import PropTypes from 'prop-types';
+import React from 'react'
+import '../styles/main.scss'
+import PropTypes from 'prop-types'
 
-const Splash = ({exitSplash, films}) => {
+const Splash = ({ exitSplash, films }) => {
   const randomNum = Math.floor(Math.random() * Math.floor(6))
   const film = films[randomNum]
-  console.log(film)
-  if(film) {
+  if (film) {
     return (
-      <section className='background'>
-            <div className="fade">
-              <div className="logo">
-                <img alt='StarWars logo'
-                    onClick={() => exitSplash()} 
-                    src="http://pngimg.com/uploads/star_wars_logo/star_wars_logo_PNG29.png" title="click to enter site"/> 
-              </div>
-              </div>
-              <div className="crawl">
-                {film.text}
-              <p>{film.title}</p>
-              <p>{film.date}</p>
-              </div>
-              <div className="button-div">
-              <button onClick={() => exitSplash()}className='enter-btn'>
-              ENTER </button>
-              </div>
-            </section>
-    )
-  } else {
-    
-    return (
-      <section className='background'>
+      <section className="background">
         <div className="fade">
+          <div className="logo">
+            <img
+              alt="StarWars logo"
+              onClick={() => exitSplash()}
+              src="http://pngimg.com/uploads/star_wars_logo/star_wars_logo_PNG29.png"
+              title="click to enter site"
+            />
+          </div>
+        </div>
+        <div className="crawl">
+          {film.text}
+          <p>{film.title}</p>
+          <p>{film.date}</p>
+        </div>
+        <div className="button-div">
+          <button onClick={() => exitSplash} className="enter-btn" type="button">ENTER</button>
         </div>
       </section>
     )
   }
+  return (
+    <section className="background">
+      <div className="fade" />
+    </section>
+  )
 }
 
 Splash.propTypes = {
-exitSplash: PropTypes.func.isRequired,
-films: PropTypes.array.isRequired
+  exitSplash: PropTypes.func.isRequired,
+  films: PropTypes.array.isRequired
 }
 
-export default Splash;
+export default Splash
