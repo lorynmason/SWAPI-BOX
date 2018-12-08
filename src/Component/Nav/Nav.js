02a882e58d9d66602bb528388459880ad30d30f9
Loryn Mason <masonloryn@gmail.com>
2018-12-08 15:45:18 -0700

    Linted Nav.js

--- src/Component/Nav/Nav.js
+++ src/Component/Nav/Nav.js
@@ -1,15 +1,17 @@
 import React from 'react'
-import '../styles/main.scss';
+import '../styles/main.scss'
 import PropTypes from 'prop-types'
 
-const Nav = ( {favorites, changePage} ) => {
-
+const Nav = ({ favorites, changePage }) => {
   return (
     <nav>
-      <button onClick={() => changePage('characters')} >Characters</button>
-      <button onClick={() => changePage('planets')}>Planets</button>
-      <button onClick={() => changePage('vehicles')}>Vehicles</button>
-      <button>Favorites: {favorites.length}</button>
+      <button type="button" onClick={() => changePage('characters')}>Characters</button>
+      <button type="button" onClick={() => changePage('planets')}>Planets</button>
+      <button type="button" onClick={() => changePage('vehicles')}>Vehicles</button>
+      <button type="button">
+        Favorites:
+        {favorites.length}
+      </button>
     </nav>
   )
 }
