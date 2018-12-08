64e735343ca579d9b1f137ffb2ac102be44b12d0
Loryn Mason <masonloryn@gmail.com>
2018-12-08 14:05:01 -0700

    linted setupTest and vehicles

--- src/Component/Vehicles.js
+++ src/Component/Vehicles.js
@@ -1,14 +1,21 @@
 import React from 'react'
-import './styles/main.scss';
+import './styles/main.scss'
 import PropTypes from 'prop-types'
 
-const Vehicles = ({vehicles}) => {
-  const cards = vehicles.map( vehicle => {
-    return (<div className="vehicle-card card">
+const Vehicles = ({ vehicles }) => {
+  const cards = vehicles.map((vehicle)=> {
+    return (
+    <div className="vehicle-card card">
       <h1>{vehicle.Name}</h1>
-      <p>Model: {vehicle.Model}</p>
-      <p>Class: {vehicle.Class}</p>
-      <p>Passengers: {vehicle.Passengers}</p>
+      <p>Model: 
+        {vehicle.Model}
+      </p>
+      <p>Class:
+         {vehicle.Class}
+      </p>
+      <p>Passengers:
+         {vehicle.Passengers}
+      </p>
    </div>)
   })
  
@@ -20,7 +27,7 @@ const Vehicles = ({vehicles}) => {
 }
 
 Vehicles.propTypes = {
-  
+  vehicles: PropTypes.array.isRequired
 }
 
 export default Vehicles
