a1fdfde6a9d04faa243284a0d56953b8dacbe791
Sara Ebbers <johndoe@example.com>
2018-12-11 13:38:50 -0700

    add second snapshot for rendering empty array

--- src/Component/Favorite.test.js
+++ src/Component/Favorite.test.js
@@ -6,6 +6,7 @@ describe('Favorites', () => {
   let wrapper
   let mockToggleFavorites
   let mockFavorites
+  // let cards
 
   beforeEach(() => {
     mockFavorites = [
@@ -29,6 +30,12 @@ describe('Favorites', () => {
   it('should call toggleFavorites with the correct params', () =>{
     wrapper.find(".favorite-btn").simulate('click')
     expect(mockToggleFavorites).toHaveBeenCalledWith(mockFavorites[0].id)
+  })
+
+  it('should render the yoda card with no favorites selected if there are no favorites AKA it shoudl match the snapshot when there are no favorites', () => {
+    mockFavorites = []
+    wrapper = shallow(<Favorites favorites={mockFavorites} toggleFavorites={mockToggleFavorites} />)
+    expect(wrapper).toMatchSnapshot()
   })
 
 })
